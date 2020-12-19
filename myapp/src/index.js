bce2e13b070e14611b0aafa412f1247106016abd
task4z <andreafl90@gmail.com>
2020-12-19 19:14:10 +0000

    firs commit

---
+++ myapp/src/index.js
@@ -0,0 +1,24 @@
+// Import React and ReactDOM libs
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create react component
+const App = () => {
+    const buttonText = { text: 'Click me'};
+    const labelText = 'Enter name:';
+    const style = { backgroundColor: 'blue', color: 'white'};
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">{labelText}</label>
+            <input id="name" type="text"/>
+            <button style={style}>
+                {buttonText.text}
+            </button>
+        </div>
+
+    );
+}
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App/>,document.querySelector('#root'));
